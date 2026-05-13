402a6808f11964908b3be7157fa8edc3f2e8b4c0
onlinewebktr <onlineweb20182019@gmail.com>
2026-05-13 19:24:49 +0530

    AK_P_6

--- school_web/Properties/AssemblyInfo.cs
+++ school_web/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("school_web")]
-[assembly: AssemblyDescription("School")]
+[assembly: AssemblyTitle("Nextronsoft School ERP")]
+[assembly: AssemblyDescription("ERP Management System")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Purnak")]
-[assembly: AssemblyProduct("school_web")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCompany("Nextronsoft")]
+[assembly: AssemblyProduct("School ERP")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.66")]
-[assembly: AssemblyFileVersion("1.0.0.66")]
+[assembly: AssemblyVersion("1.0.0.67")]
+[assembly: AssemblyFileVersion("1.0.0.67")]
 [assembly: NeutralResourcesLanguageAttribute("")]
